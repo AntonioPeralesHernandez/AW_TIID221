46906f910d64c3331a08a5265a78e3b9eff16a63
AntonioPeralesHernandez <123046676@upq.edu.mx>
2026-09-17 15:33:54 -0600

    Commit xampp

---
+++ index.php
@@ -0,0 +1,28 @@
+<!--<?php
+  echo "Hola Mundo"
+?>  -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: blue;
+        
+        }
+    </style>
+
+</head>
+<body>
+ 
+<h1>Hola mundo de Antonio</h1>
+<h2>Hola mundo de Antonio</h2>
+<h3>Hola mundo de Antonio</h3>
+<h4>Hola mundo de Antonio</h4>
+<h5>Hola mundo de Antonio</h5>
+
+</body>
+</html>
