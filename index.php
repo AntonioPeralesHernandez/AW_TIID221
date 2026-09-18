3055b35fad0a3bfe1b92a442bb8d7ef125db63c1
AntonioPeralesHernandez <123046676@upq.edu.mx>
2026-09-18 17:48:59 -0600

    Practica de index y styles

--- index.php
+++ index.php
@@ -8,21 +8,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        h1{
-            color: blue;
-        
-        }
-    </style>
+
+ <link rel="stylesheet" href="styles.css">
 
 </head>
 <body>
  
+<div>
 <h1>Hola mundo de Antonio</h1>
-<h2>Hola mundo de Antonio</h2>
-<h3>Hola mundo de Antonio</h3>
-<h4>Hola mundo de Antonio</h4>
-<h5>Hola mundo de Antonio</h5>
+<h2>Antonio tiene 21 años</h2>
+<h3>Se encuntra estudiando TIID</h3>
+<h4>Le gusta jugar videojuegos</h4>
+<h5>Tiene un perro </h5>
+
+<p>Estoy aprendiendo desarrollo web con HTML, CSS Y PHP.</P>
+
+</div>
+
+  <div>
+    <h2>Mi sección</h2>
+
+    <p>Este es el contenido de mi sección.</p>
+</div>
+
+<div class="seccion">
+        <h2>Título del párrafo</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet,
+        </p>
+    </div>
+
+    <div class="seccion2">
+        <h2>Diego es mi bro</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet
+        </p>
+    </div>
 
 </body>
 </html>
